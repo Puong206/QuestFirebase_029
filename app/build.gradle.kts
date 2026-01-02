b9c4bbece5beb41a0a250eb8edbd9ea2e6be03f2
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-02 10:21:44 +0700

    Menambah dependensi Firebase dan Coroutines Play Services

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,12 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.analytics)
+    implementation(libs.coroutines.play.services)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
