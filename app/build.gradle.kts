202dede4882b0966ce48969b318926482c37b7bd
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-02 10:22:13 +0700

    Menambah dependensi Navigation Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,8 @@ dependencies {
     implementation(libs.firebase.analytics)
     implementation(libs.coroutines.play.services)
 
+    implementation(libs.navigation.compose)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
