989ab3e7db3d8e449f1fb66d7a999ada4d8736dc
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-09 21:11:51 +0700

    Menambahkan rute navigasi untuk Detail Siswa
    Menambahkan `composable` untuk `DestinasiDetail` ke dalam `PetaNavigasi` agar aplikasi dapat berpindah ke layar Detail Siswa dengan argumen ID tertentu.

--- app/src/main/java/com/example/myfirebase/view/controlNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/myfirebase/view/controlNavigasi/PetaNavigasi.kt
@@ -8,9 +8,11 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.example.myfirebase.view.DetailSiswaScreen
 import com.example.myfirebase.view.EntrySiswaScreen
 import com.example.myfirebase.view.HomeScreen
 import com.example.myfirebase.view.route.DestinasiDetail
+import com.example.myfirebase.view.route.DestinasiEdit
 import com.example.myfirebase.view.route.DestinasiEntry
 import com.example.myfirebase.view.route.DestinasiHome
 
@@ -39,6 +41,15 @@ fun HostNavigasi(
             EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route)
                 })
             }
+        composable(DestinasiDetail.routeWithArgs, arguments = listOf(navArgument
+            (DestinasiDetail.itemIdArg) {
+            type = NavType.StringType })
+        ){
+            DetailSiswaScreen(navigateToEditItem = {navController.navigate("${DestinasiEdit
+                .route}/${it}")},
+                navigateBack = { navController.navigate(DestinasiHome.route)})
+        }
+
         }
     }
 
