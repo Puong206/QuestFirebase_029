c99d2e1fa7c6c7a7570385420dcb3e65e92a7cc2
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-09 20:18:06 +0700

    Menambahkan DetailViewModel
    Mengimplementasikan class DetailViewModel yang menerima SavedStateHandle dan RepositorySiswa sebagai parameter.

--- app/src/main/java/com/example/myfirebase/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/DetailViewModel.kt
@@ -1,9 +1,17 @@
 package com.example.myfirebase.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.myfirebase.modeldata.Siswa
+import com.example.myfirebase.repositori.RepositorySiswa
 
 sealed interface StatusUIDetail {
     data class Success(val satusiswa: Siswa?) : StatusUIDetail
     object Error : StatusUIDetail
     object Loading : StatusUIDetail
+}
+
+class DetailViewModel(savedStateHandle: SavedStateHandle, private val repositorySiswa: RepositorySiswa):
+    ViewModel() {
+
 }
