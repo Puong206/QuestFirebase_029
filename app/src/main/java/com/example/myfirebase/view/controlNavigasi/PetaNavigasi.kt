27805e8915f68246cce259c360d31ec3ef21ff4c
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-09 21:14:17 +0700

    Menambahkan rute navigasi untuk layar Edit Siswa
    Menambahkan composable DestinasiEdit ke dalam PetaNavigasi untuk menangani perpindahan ke layar EditSiswaScreen dengan argumen tertentu.

--- app/src/main/java/com/example/myfirebase/view/controlNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/myfirebase/view/controlNavigasi/PetaNavigasi.kt
@@ -9,6 +9,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.example.myfirebase.view.DetailSiswaScreen
+import com.example.myfirebase.view.EditSiswaScreen
 import com.example.myfirebase.view.EntrySiswaScreen
 import com.example.myfirebase.view.HomeScreen
 import com.example.myfirebase.view.route.DestinasiDetail
@@ -49,7 +50,12 @@ fun HostNavigasi(
                 .route}/${it}")},
                 navigateBack = { navController.navigate(DestinasiHome.route)})
         }
-
+        composable(DestinasiEdit.routeWithArgs, arguments = listOf(navArgument
+            (DestinasiEdit.itemIdArg){
+                type = NavType.StringType})) {
+            EditSiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route)},
+                onNavigateUp = { navController.navigateUp()})
+        }
         }
     }
 
