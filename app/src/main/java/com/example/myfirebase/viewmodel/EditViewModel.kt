381c3ef09db81ee6c7d2921fce4fcce3c10bb0c2
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-09 20:46:14 +0700

    Menambahkan fungsi validasi input pada EditViewModel
    Menambahkan fungsi `validasiInput` untuk memastikan data nama, alamat, dan telepon tidak kosong sebelum diproses.

--- app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.myfirebase.modeldata.DetailSiswa
 import com.example.myfirebase.modeldata.UIStateSiswa
 import com.example.myfirebase.modeldata.toUIStateSiswa
 import com.example.myfirebase.repositori.RepositorySiswa
@@ -22,6 +23,12 @@ class EditViewModel(savedStateHandle: SavedStateHandle, private val repositorySi
         viewModelScope.launch {
             uiStateSiswa = repositorySiswa.getSatuSiswa(idSiswa)!!
                 .toUIStateSiswa()
+        }
+    }
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
         }
     }
 }
