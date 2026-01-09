a118ac7220562031ce57902b303ffe573fc01128
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-01-09 21:17:27 +0700

    Menambahkan import Padding di HalamanEdit.kt

--- app/src/main/java/com/example/myfirebase/view/HalamanEdit.kt
+++ app/src/main/java/com/example/myfirebase/view/HalamanEdit.kt
@@ -1,5 +1,6 @@
 package com.example.myfirebase.view
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
